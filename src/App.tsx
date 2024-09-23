14db474ada121320ba81cb11d6ebf7716c979ce3
jeffreyvnzanten <j.van.zanten@gmail.com>
2024-09-23 21:49:34 +0200

    updated met syntax opties

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const App: React.FC = () => {
+function App() {
+  return <h1>Hello, World!</h1>;
+}
+
+/* Andere syntax
+
+const App = () => {
   return <h1>Hello, World!</h1>;
 };
+
+*/
 
 export default App;
